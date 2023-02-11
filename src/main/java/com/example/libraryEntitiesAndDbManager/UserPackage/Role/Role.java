2470aee27f289b9bc0314a286f930aae62a67e63
saveligulas <saveli.gulas@gmail.com>
2023-02-11 22:04:58 +0100

    new Role

--- src/main/java/com/example/libraryEntitiesAndDbManager/UserPackage/Role/Role.java
+++ src/main/java/com/example/libraryEntitiesAndDbManager/UserPackage/Role/Role.java
@@ -1,4 +1,24 @@
 package com.example.libraryEntitiesAndDbManager.UserPackage.Role;
 
+import com.example.libraryEntitiesAndDbManager.UserPackage.User.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @OneToMany(targetEntity = User.class, mappedBy = "role",
+            fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<User> users;
+
 }
