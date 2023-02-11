3116e0e2f1def9ce9ed62d5f8394237acfbf238e
saveligulas <saveli.gulas@gmail.com>
2023-02-11 22:05:17 +0100

    new User

--- src/main/java/com/example/libraryEntitiesAndDbManager/UserPackage/User/User.java
+++ src/main/java/com/example/libraryEntitiesAndDbManager/UserPackage/User/User.java
@@ -1,4 +1,32 @@
 package com.example.libraryEntitiesAndDbManager.UserPackage.User;
 
+import com.example.libraryEntitiesAndDbManager.LibraryContentsPackage.Book.Book;
+import com.example.libraryEntitiesAndDbManager.UserPackage.Role.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate dob;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+    @OneToMany(targetEntity = Book.class, mappedBy = "user",
+            fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Book> books;
 }
